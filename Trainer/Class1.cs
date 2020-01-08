82c3396ac0b76bc8f3a6e3640c5e5ec332b67c75
samratrocks <samrat.shiwakoti@gmail.com>
2020-01-07 19:57:36 -0500

    Add project files.

---
+++ Trainer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainer
+{
+    public class Class1
+    {
+    }
+}
