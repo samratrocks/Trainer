233fab75f7f8717911c027a01bca5835f8ce7c66
samratrocks <samrat.shiwakoti@gmail.com>
2020-01-07 20:01:49 -0500

    add Trainer class.

---
+++ Trainer/Trainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using System.Windows.Forms;
+
+namespace Trainer
+{
+    public class Trainer: Script
+    {
+        public Trainer()
+        {
+            Tick += onTick;
+            KeyDown += onKeyDown;
+            KeyUp += onKeyUp;
+
+            renderUI();
+        }
+
+        // TODO: Create a UI Menu using the API
+        private void renderUI()
+        {
+
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
